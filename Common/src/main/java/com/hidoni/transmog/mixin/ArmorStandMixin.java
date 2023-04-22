8b773c808d41402bdaa1e17af958bee03a98c1db
Hidoni <git@hidoni.com>
2023-04-23 00:50:57 +0300

    Add ArmorStandMixin to fix Armor Stands rendering original item

---
+++ Common/src/main/java/com/hidoni/transmog/mixin/ArmorStandMixin.java
@@ -0,0 +1,21 @@
+package com.hidoni.transmog.mixin;
+
+import com.hidoni.transmog.RenderUtils;
+import com.hidoni.transmog.TransmogUtils;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ArmorStand.class)
+public class ArmorStandMixin {
+    @Inject(method = "getItemBySlot", at=@At("RETURN"), cancellable = true)
+    private void transmogItemBySlot(EquipmentSlot slot, CallbackInfoReturnable<ItemStack> cir) {
+        if (RenderUtils.isCalledForRendering() && !RenderUtils.isCalledForInventory() && TransmogUtils.isItemStackTransmogged(cir.getReturnValue())) {
+            cir.setReturnValue(TransmogUtils.getAppearanceItemStack(cir.getReturnValue(), false));
+        }
+    }
+}
