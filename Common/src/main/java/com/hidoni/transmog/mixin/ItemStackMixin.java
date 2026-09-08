6b41e7630d62bf29411196a9a1241d6540394070
Hidoni <git@hidoni.com>
2026-09-08 23:03:46 +0300

    Remove unused imports in ItemStackMixin

--- Common/src/main/java/com/hidoni/transmog/mixin/ItemStackMixin.java
+++ Common/src/main/java/com/hidoni/transmog/mixin/ItemStackMixin.java
@@ -1,32 +1,19 @@
 package com.hidoni.transmog.mixin;
 
-import com.hidoni.transmog.TransmogUtils;
 import com.hidoni.transmog.config.Config;
 import com.hidoni.transmog.config.TooltipDetailLevel;
-import com.hidoni.transmog.i18n.TranslationKeys;
 import com.hidoni.transmog.registry.ModDataComponents;
-import net.minecraft.ChatFormatting;
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
